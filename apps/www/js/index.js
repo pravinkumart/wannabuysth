d79fd6bb2457ec1d0a577be6949c8a37601fa12a
1314520z <hell122@163.com>
2013-08-27 22:45:32 +0800

    0

--- apps/www/js/index.js
+++ apps/www/js/index.js
@@ -1,6 +1,7 @@
+var index_page = {}
 $(document).ready(function() {
-    //help_init() //帮助页面
-	accounts_init() //登录注册选择页面
+    help_init() //帮助页面nts_init() //登录注册选择页面
+	//index_init() 
 });
 
 $(document).bind('pageinit', function() {
@@ -10,21 +11,44 @@ $(document).bind('pageinit', function() {
 $(document).bind('pageshow', function() {
 	
 });
+function gopage(page_id){
+	$.mobile.changePage(page_id,{transition:'slide'})
+}
 function help_init(){
-   // $('#help').show();	
-   $.mobile.changePage('#help');
-   return 
-  var obj = $('#wrapper_help');
-  var width = $(window).width();
+	 $('#help_href').click();
+}
+
+$("#index").live("pagecreate",function(){
+ 	// index_init()
+})
+$("#index").bind('pageshow', function() {
+	index_init()
+	// myScroll.refresh()
+});
+function index_init(){
+  get_index_types_data()
+  var obj = $('#wrapper_index');
+  var width = 230;
   var count = obj.find('.thelist li').length;
   	  obj.find('.thelist li').width(width);
   	  obj.find('.scroller').width(count*width);
-  var myScroll = new iScroll('wrapper_help',{
+  if(index_page.myScroll){
+  		index_page.myScroll.refresh()
+  		return false;
+  	}
+  var myScroll = new iScroll('wrapper_index',{
     snap: true,
     momentum: false,
     hScrollbar: false
   });
- 
+  index_page.myScroll = myScroll;
+}
+
+//首页类型数据填充
+function get_index_types_data(){
+	var html = ''
+	
+	
 }
 
 function accounts_init(){
