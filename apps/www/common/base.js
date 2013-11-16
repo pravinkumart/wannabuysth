da3c0ec6a85ec8054cf57e99b70a67c276fbe08e
1314520z <hell122@163.com>
2013-11-16 22:50:51 +0800

    admin

--- apps/www/common/base.js
+++ apps/www/common/base.js
@@ -2,13 +2,24 @@ var www = 'http://app.bangban.com';
 var $ = jQuery;
 $.mobile.allowCrossDomainPages=true;
 
+Raven.config('http://53b9b800c28741fda2fda2107f96dbfc@error.codoon.com/3', {
+    whitelistUrls: []
+}).install();
+
+Raven.setUser({
+    email: '155214685@qq.com',
+    id: '3'
+});
+Raven.captureMessage('base js 读取成功');
 if(navigator.onLine==false){
+	Raven.captureMessage('网络未连接,点击屏幕，重新加载  navigator.onLine=false');
 	$('#loading .tc').html('网络未连接,点击屏幕，重新加载');
 	$('#loading').fastClick(function(){
 		window.location.href = window.location.href;
 	});
 }
 else{
+	Raven.captureMessage('现在执行loading state ');
 	$("#loading").live('pageshow', function() {
 		$.mobile.loading('show', {text : 'test', theme : 'a'});
 		$.ajax({
@@ -25,17 +36,17 @@ else{
 		});
 	});
 }
-
+Raven.captureMessage('现在执行 加载 服务端 index.js ');
 (function(win){
 	var e = document.createElement("script");
 	e.setAttribute("src",www+'static/js/index.js?v='+(new Date()).getTime());
 	document.getElementsByTagName("body")[0].appendChild(e);
-	
+	Raven.captureMessage('服务端 index.js 加载代码完成');
 	var e = document.createElement("link");
 	e.setAttribute("rel",'stylesheet');
 	e.setAttribute("href",www+'static/css/index.css?v='+(new Date()).getTime());
 	document.getElementsByTagName("body")[0].appendChild(e);
-	
+	Raven.captureMessage('服务端 index.css 加载代码完成');
 	
 	if(www.indexOf('5000')>-1){
 		var e = document.createElement("script");
