454a905d203a02447ab42383543b87905fc0c44f
1314520z <hell122@163.com>
2013-11-27 00:07:29 +0800

    1

---
+++ android_app/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+E:\workspace\wannabuysth\android_app\platforms\android\gen\com\miaomi\bangban\R.java \
+ : E:\workspace\wannabuysth\android_app\platforms\android\res\drawable\icon.png \
+E:\workspace\wannabuysth\android_app\platforms\android\res\drawable-hdpi\icon.png \
+E:\workspace\wannabuysth\android_app\platforms\android\res\drawable-ldpi\icon.png \
+E:\workspace\wannabuysth\android_app\platforms\android\res\drawable-mdpi\icon.png \
+E:\workspace\wannabuysth\android_app\platforms\android\res\drawable-xhdpi\icon.png \
+E:\workspace\wannabuysth\android_app\platforms\android\res\values\strings.xml \
+E:\workspace\wannabuysth\android_app\platforms\android\res\xml\config.xml \
+E:\workspace\wannabuysth\android_app\platforms\android\bin\AndroidManifest.xml \
---
+++ android_app/platforms/android/gen/com/miaomi/bangban/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.miaomi.bangban;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
