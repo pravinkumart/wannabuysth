aa135e7c64f7f0ecd977d3b734b9a4a54fc15d24
1314520z <hell122@163.com>
2013-11-29 23:34:33 +0800

    1

--- wannabuysth/static/js/base.js
+++ wannabuysth/static/js/base.js
@@ -21,10 +21,5 @@ else{
 	e.setAttribute("rel",'stylesheet');
 	e.setAttribute("href",www+'static/css/index.css?v='+(new Date()).getTime());
 	document.getElementsByTagName("body")[0].appendChild(e);
-	
-	
-	var e = document.createElement("script");
-	e.setAttribute("src","http://192.168.1.120:8080/target/target-script-min.js#anonymous");
-	document.getElementsByTagName("body")[0].appendChild(e);
 
 })(window);
